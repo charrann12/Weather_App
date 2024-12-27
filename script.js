ef634f54ff9e48eda489ab3ed2ba0b89b913a8a1
Charrann12 <nethisaicharan12345@gmail.com>
2024-12-27 22:48:19 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,76 @@
+
+// apiKey taken from open weather map 
+const apiKey = "53a91e6b198a1c295aa4785ece448e95";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+//these are made to functionalise the input and search button.
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search  button");
+
+// intialising weather-icon so that the image in the card changes as per the weather condition of the city
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+ //function to fetch weather conditions from the apiKey
+async function checkWeather(city) {
+     const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+     if (response.status == 404){
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "none";
+     }
+     else{
+      var data = await response.json();
+
+     
+    // to manipulate the current info as per the input city's forecasting info
+     document.querySelector(".city_name").innerHTML = data.name;
+     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+     document.querySelector(".wind").innerHTML = data.wind.speed + " Kmph";
+
+    // changing the display none to block whenever the city name is entered 
+     document.querySelector(".weather ").style.display = "block";
+
+     // image changes 
+     if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+     }
+
+     else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+     }
+
+     else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+     }
+
+     else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+     }
+
+     else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+     }
+
+     else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png";
+     }
+      
+     document.querySelector(".weather").style.display = "block";
+     document.querySelector(".error").style.display="none";
+
+
+     }
+      
+
+}
+
+// event listener for search button to fetch the weather of the city entered in the search box 
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
+
+//function calling
+checkWeather();
+
