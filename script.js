5c82aab04c6f074bd0fd28a081c8c0b3d7149f60
Sai Charan Nethi <158495761+charrann12@users.noreply.github.com>
2024-12-28 23:41:34 +0530

    Bg Video Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const searchBtn = document.querySelector(".search  button");
 // intialising weather-icon so that the image in the card changes as per the weather condition of the city
 const weatherIcon = document.querySelector(".weather-icon");
 
+const background = document.querySelector("#myVideo");
+
 
  //function to fetch weather conditions from the apiKey
 async function checkWeather(city) {
@@ -34,27 +36,34 @@ async function checkWeather(city) {
      // image changes 
      if(data.weather[0].main == "Clouds"){
         weatherIcon.src = "images/clouds.png";
+        background.src = "videos/cloudy.mp4";
      }
 
      else if(data.weather[0].main == "Clear"){
         weatherIcon.src = "images/clear.png";
+        background.src = "videos/clear.mp4";
      }
 
      else if(data.weather[0].main == "Drizzle"){
         weatherIcon.src = "images/drizzle.png";
+        background.src = "videos/drizzle.mp4";
      }
 
      else if(data.weather[0].main == "Mist"){
         weatherIcon.src = "images/mist.png";
-     }
+        background.src = "videos/mistt.mp4";
+      }
 
      else if(data.weather[0].main == "Rain"){
         weatherIcon.src = "images/rain.png";
+        background.src = "videos/rain.mp4";
      }
 
      else if(data.weather[0].main == "Snow"){
         weatherIcon.src = "images/snow.png";
+        background.src = "videos/snow.mp4";
      }
+
       
      document.querySelector(".weather").style.display = "block";
      document.querySelector(".error").style.display="none";
